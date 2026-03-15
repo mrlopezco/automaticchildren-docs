c36d20cb6c56971f522561b4c2ec344770163e44
Ignacio Lopez Coll <joseignaciolopezcoll@gmail.com>
2026-03-14 18:55:26 -0600

    Update documentation to include multi-select work items
    - Added a new section on multi-select work items in the Getting Started and Concepts pages.
    - Updated the index to reflect the addition of multi-select work items in the concepts overview.

--- content/concepts/_meta.js
+++ content/concepts/_meta.js
@@ -1,6 +1,7 @@
 export default {
 	"general-rules": "General Rules",
 	cascades: "Cascades",
+	"multi-select": "Multi-select work items",
 	placeholders: "Placeholders",
 	licensing: "Licensing",
 };
